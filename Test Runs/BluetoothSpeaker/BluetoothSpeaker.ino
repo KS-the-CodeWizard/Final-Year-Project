98244cf6e924e2a91b78399894f3fde9907cf943
KS-the-CodeWizard <154646525+KS-the-CodeWizard@users.noreply.github.com>
2024-02-19 07:05:03 +0530

    New test file

---
+++ Test Runs/BluetoothSpeaker/BluetoothSpeaker.ino
@@ -0,0 +1,18 @@
+#include "BluetoothA2DPSink.h"
+
+BluetoothA2DPSink a2dp_sink;
+
+void setup() {
+    i2s_pin_config_t my_pin_config = {
+        .mck_io_num = I2S_PIN_NO_CHANGE,
+        .bck_io_num = 26,
+        .ws_io_num = 25,
+        .data_out_num = 22,
+        .data_in_num = I2S_PIN_NO_CHANGE
+    };
+    a2dp_sink.set_pin_config(my_pin_config);
+    a2dp_sink.start("MyMusic");
+}
+
+void loop() {
+}
